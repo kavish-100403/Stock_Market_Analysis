75c5ec491c82f9c0a6a658c2b952aa57f7fe6451
kavish-100403 <rkpopat00@gmail.com>
2024-09-15 18:33:55 +0530

    Programmed this file to run polynomial regression for a particular stock file as of now which is apple, and the file contains regression for the opening and closing price of the stock and the accuracy for both the cases is evaluated using r-squared

---
+++ polynomial_regression.py
@@ -0,0 +1,67 @@
+# Polynomial Regression
+def open_polynomial_regression():
+
+    # Importing the libraries
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
+    # Importing the dataset
+    dataset = pd.read_csv('AAPL_stock_data.csv')
+    X = dataset.iloc[:, [i for i in range(dataset.shape[1]) if i not in [0, 1]]].values
+    y = dataset.iloc[:, 1].values
+
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+    # Training the Polynomial Regression model on the Training set
+    from sklearn.preprocessing import PolynomialFeatures
+    from sklearn.linear_model import LinearRegression
+    poly_reg = PolynomialFeatures(degree = 4)
+    X_poly = poly_reg.fit_transform(X_train)
+    regressor = LinearRegression()
+    regressor.fit(X_poly, y_train)
+
+    # Predicting the Test set results
+    y_pred = regressor.predict(poly_reg.transform(X_test))
+    np.set_printoptions(precision=13)
+    print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+    # Evaluating the Model Performance
+    from sklearn.metrics import r2_score
+    print(r2_score(y_test, y_pred)*100)
+
+
+def close_polynomial_regression():
+
+    # Importing the libraries
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
+    # Importing the dataset
+    dataset = pd.read_csv('AAPL_stock_data.csv')
+    X = dataset.iloc[:, [i for i in range(dataset.shape[1]) if i not in [0, 4]]].values
+    y = dataset.iloc[:, 4].values
+
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+    # Training the Polynomial Regression model on the Training set
+    from sklearn.preprocessing import PolynomialFeatures
+    from sklearn.linear_model import LinearRegression
+    poly_reg = PolynomialFeatures(degree = 4)
+    X_poly = poly_reg.fit_transform(X_train)
+    regressor = LinearRegression()
+    regressor.fit(X_poly, y_train)
+
+    # Predicting the Test set results
+    y_pred = regressor.predict(poly_reg.transform(X_test))
+    np.set_printoptions(precision=13)
+    print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+    # Evaluating the Model Performance
+    from sklearn.metrics import r2_score
+    print(r2_score(y_test, y_pred)*100)
