5afca8c7124d15991fb33911cbf3f35bb66c74b8
kavish-100403 <rkpopat00@gmail.com>
2024-09-20 16:09:15 +0530

    in frame four, the model will now run after taking the input which are stock name, start date and end date from the user and the regression model will run after displaying fourth frame and added functionality that the generated csv file will be auto deleted once the tkinter program is closed

--- mainfile.py
+++ mainfile.py
@@ -10,9 +10,10 @@ from random_forest_regression import close_random_forest_regression as crfr
 from support_vector_regression import open_support_vector_regression as osvr
 from support_vector_regression import close_support_vector_regression as csvr
 
+csv_filename=None
 # Evaluating the best performance of open market from all the models
 def eval_open_model():
-    open_models=[odtr, omlr, opr, orfr, osvr]
+    open_models=[lambda:odtr(csv_filename), lambda: omlr(csv_filename), lambda: opr(csv_filename), lambda: orfr(csv_filename), lambda: osvr(csv_filename)]
     best_open_model=None
     best_open_rms = float('inf')
     best_open_model_name=""
@@ -28,11 +29,12 @@ def eval_open_model():
             best_open_model_name = model_name
 
     # Print the best model's performance
-    print(f"\nBest Open Model: {best_open_model_name}, RMS: {best_open_rms}")
+    # print(f"\nBest Open Model: {best_open_model_name}, RMS: {best_open_rms}")
+    return best_open_model_name, best_open_rms
 
 
 def eval_close_model():
-    close_models=[cdtr, cmlr, cpr, crfr, csvr]
+    close_models=[lambda:cdtr(csv_filename), lambda: cmlr(csv_filename), lambda: cpr(csv_filename), lambda: crfr(csv_filename), lambda: csvr(csv_filename)]
     best_close_model=None
     best_close_rms = float('inf')
     best_close_model_name=""
@@ -48,14 +50,27 @@ def eval_close_model():
             best_close_model_name = model_name
 
     # Print the best model's performance
-    print(f"\nBest close Model: {best_close_model_name}, RMS: {best_close_rms}")
+    # print(f"\nBest close Model: {best_close_model_name}, RMS: {best_close_rms}")
+    return best_close_model_name, best_close_rms
+
+# Generating CSV File from the input of the user
+def generate_csv_and_evaluate(ticker, start_date, end_date, controller):
+    global csv_filename
+    data_generation.dataGeneration(ticker, start_date, end_date)
+    csv_filename = f"{ticker}_stock_data.csv"
+    controller.show_frame(PageFour)
+
+
+                                    #Start of the Tkinter
 
 
 
 from tkcalendar import DateEntry
 import tkinter as tk
 from tkinter import ttk
+import os
 
+# Page 1.. basic name of the Project 
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -65,6 +80,8 @@ class PageOne(tk.Frame):
                             command=lambda: controller.show_frame(PageTwo))
         button.pack()
 
+
+# Page 2 will have the stock selection and Start date and End Date, so User can select according to their preference
 class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -121,10 +138,11 @@ class PageTwo(tk.Frame):
         button = ttk.Button(self, text="Confirm",
                             command=lambda:[
                                             update_date_var(), 
-                                            data_generation.dataGeneration(ticker=selected_stock_name_var.get(), start_date=selected_start_date_var.get(),end_date=selected_end_date_var.get()),
-                                            eval_open_model,
-                                            eval_close_model,
-                                            controller.show_frame(PageFour)
+                                            generate_csv_and_evaluate(
+                                                selected_stock_name_var.get(),
+                                                selected_start_date_var.get(),
+                                                selected_end_date_var.get(),
+                                                controller)
                                             ])
         button.pack()
         button.place(x=220, y=200)
@@ -151,46 +169,92 @@ class PageThree(tk.Frame):
         button.pack()
         button.place(x=400, y=200)
 
+# Page 4 will display the best accuracy of all the stock and will display the graph of the best stock and also the accuracy and graph of other stocks
 class PageFour(tk.Frame):
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        self.controller = controller
 
-
-        label = tk.Label(self, text="The best accuracy for \n opening price was obtained by: ", font=("Arial",10))
+# Label & Text widget to display the best model for open price name 
+        label = tk.Label(self, text="The best accuracy for \n opening price was obtained by: ", font=("Arial", 10))
         label.pack(pady=10, padx=10)
         label.place(x=0, y=0)
+        self.best_open_accuracy_name = tk.Text(self, height=1.5, width=20, font=("Arial", 12))
+        self.best_open_accuracy_name.place(x=5, y=40)
+        # Adding a placeholder before the actual data is inserted
+        self.best_open_accuracy_name.insert(1.0, "Evaluating...")
 
-        # Here will be the name of the model which showed best accuracy
-
-        # This is show the label of the Accuracy of the best model
-        label = tk.Label(self, text="Accuracy %: ", font=("Arial",10))
+# Label & Text widget to display the best model for open price accuracy
+        label = tk.Label(self, text="Accuracy %:", font=("Arial", 10))
         label.pack(pady=10, padx=10)
         label.place(x=0, y=100)
+        self.best_open_accuracy_rms = tk.Text(self, height=1.5, width=20, font=("Arial", 12))
+        self.best_open_accuracy_rms.place(x=5, y=125)
+        # Adding a placeholder before the actual data is inserted
+        self.best_open_accuracy_rms.insert(1.0, "Evaluating...")
 
-        # Here will be the text box which will show the accuracy of the model which performed the best
+# Label & Text widget to display the best model for Close price name
+        label = tk.Label(self, text="The best accuracy for \n closing price was obtained by: ", font=("Arial", 10))
+        label.pack(pady=10, padx=10)
+        label.place(x=280, y=0)
+        self.best_close_accuracy_name = tk.Text(self, height=1.5, width=20, font=("Arial", 12))
+        self.best_close_accuracy_name.place(x=288, y=40)
+        # Adding a placeholder before the actual data is inserted
+        self.best_close_accuracy_name.insert(1.0, "Evaluating...")
+
+# Label & Text widget to display the best model for close price accuracy
+        label = tk.Label(self, text="Accuracy %:", font=("Arial", 10))
+        label.pack(pady=10, padx=10)
+        label.place(x=280, y=100)
+        self.best_close_accuracy_rms = tk.Text(self, height=1.5, width=20, font=("Arial", 12))
+        self.best_close_accuracy_rms.place(x=288, y=125)
+        # Adding a placeholder before the actual data is inserted
+        self.best_close_accuracy_rms.insert(1.0, "Evaluating...")
+
+#Start Button for the Model Evaluation [Might use later]
+        # button = ttk.Button(self, text="Confirm",
+        #                     command=lambda:[controller.show_frame(PageFour),
+        #                                     self.on_show_frame()
+        #                                     ])
+        # button.pack()
+        # button.place(x=400, y=200)
+
+
+# To be done: Run the model after opening the fourth frame, as of now it is running before
+    def on_show_frame(self):
+        # This method will be called every time the frame is raised or shown
+
+        # Running the model evaluations when the frame is displayed
+        best_open_model_name, best_open_rms = eval_open_model()
+        best_close_model_name, best_close_rms = eval_close_model()
+
+        # Inserting the best open model name into the Text widget
+        self.best_open_accuracy_name.delete(1.0, tk.END)
+        self.best_open_accuracy_name.insert(1.0, best_open_model_name)
+
+        # Inserting the best open model accuracy into the Text widget
+        self.best_open_accuracy_rms.delete(1.0, tk.END)
+        self.best_open_accuracy_rms.insert(1.0, best_open_rms)
+
+        # Inserting the best close model name into the Text widget
+        self.best_close_accuracy_name.delete(1.0, tk.END)
+        self.best_close_accuracy_name.insert(1.0, best_close_model_name)
+
+        # Inserting the best close model accuracy into the Text widget
+        self.best_close_accuracy_rms.delete(1.0, tk.END)
+        self.best_close_accuracy_rms.insert(1.0, best_close_rms)
 
         # Here will be the button which will show the graph of the best model whose x-axis will be date and y-axis will be the opening or closing price
 
         # Here will be the button which will show the graph of the other models whose x-axis will be date and y-axis will be the opening or closing price
 
-
-        label = tk.Label(self, text="The best accuracy for \n closing price was obtained by: ", font=("Arial",10))
-        label.pack(pady=10, padx=10)
-        label.place(x=220, y=0)
-        # Here will b the name of the model which showed best accuracy
-
-        # This is show the label of the Accuracy of the best model
-        label = tk.Label(self, text="Accuracy %: ", font=("Arial",10))
-        label.pack(pady=10, padx=10)
-        label.place(x=220, y=100)
-
-        # Here will be the text bix which will show the accuracy of the model which performed the best
-
-        # Here will be the button which will show the graph of the best model whose x-axis will be date and y-axis will be the opening or closing price
-
-        # Here will be the button which will show the graph of the other models whose x-axis will be date and y-axis will be the opening or closing price
-
-
+def on_closing():
+    global csv_filename
+    if csv_filename and os.path.exists(csv_filename):
+        os.remove(csv_filename)  # Delete the CSV file
+        print(f"CSV file '{csv_filename}' deleted!")
+    app.destroy()
 
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -218,13 +282,16 @@ class App(tk.Tk):
         elif cont == PageTwo:
             self.geometry("525x230")  # Set window size for Page 2
         elif cont == PageThree:
-            self.geometry("525x230")  # Set window size for Page 2
+            self.geometry("525x230")  # Set window size for Page 3
         elif cont == PageFour:
-            self.geometry("525x230")  # Set window size for Page 2
+            self.geometry("525x230")  # Set window size for Page 4
+            # This is used to start the evaluation of models after displaying fourth frames
+            frame.on_show_frame()
 
 if __name__ == "__main__":
     app = App()
-    app.mainloop()
+    app.protocol("WM_DELETE_WINDOW", on_closing) 
+    app.mainloop()                                                                                                                                                                                                                                                                                                                                                                                          
     
 
 
